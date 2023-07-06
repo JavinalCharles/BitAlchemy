22dc37856ba84744d1e902999968c40aa5019c7a
Charlemagne Javinal <javinal.charlemagne@gmail.com>
2023-07-06 14:21:23 +0800

    Added new added files for samples.

---
+++ dirs/bit-alchemy/assets/Textures/oak_woods_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="oak_woods_tileset" tilewidth="24" tileheight="24" tilecount="315" columns="21">
+ <image source="oak_woods_v1.0/oak_woods_tileset.png" width="504" height="360"/>
+</tileset>
