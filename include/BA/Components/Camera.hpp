689760b21453126bd617479aa8d9b1d04ed2d1ef
Charlemagne Javinal <javinal.charlemagne@gmail.com>
2023-07-23 21:24:03 +0800

    New Camera Component and CameraSystem component system.

---
+++ include/BA/Components/Camera.hpp
@@ -0,0 +1,21 @@
+#pragma once 
+
+#include "BA/Components/Component.hpp"
+#include "BA/Window/View.hpp"
+
+namespace ba {
+
+class Camera : public Component {
+public:
+	Camera(Entity* owner);
+
+	void setView(const View& view);
+	const View& getView() const;
+	void setViewCenter(const Vector2f& center);
+private:
+	ba::View m_cameraView;
+public:
+	static const IDtype CID;
+}; // class Camera
+
+} // namespace 
---
+++ src/BA/Systems/CameraSystem.cpp
@@ -0,0 +1,29 @@
+#include "BA/Systems/CameraSystem.hpp"
+#include "BA/Systems/EntityManager.hpp"
+#include "BA/Entities/Entity.hpp"
+
+namespace ba {
+
+CameraSystem::CameraSystem(EntityManager* entityManager) :
+	ComponentSystem::ComponentSystem(entityManager)
+{
+
+}
+
+void CameraSystem::update(float) {
+	for (auto& ID : this->getIDs()) {
+		auto e = this->getEntity(ID);
+		auto c = e->getComponent<Camera>();
+
+		c->setViewCenter(e->getPosition());
+		e->CONTEXT->window->setView(c->getView());
+	}
+}
+
+void CameraSystem::add(std::shared_ptr<Entity>& entity) {
+	if (entity->getComponent<Camera>() != nullptr) {
+		this->addID(entity->ID);
+	}
+}
+
+} // namespace ba
