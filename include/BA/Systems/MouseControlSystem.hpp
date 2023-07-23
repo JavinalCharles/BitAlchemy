e823cc59c3dc5bf7cba574402a3037ae79224d0a
Charlemagne Javinal <javinal.charlemagne@gmail.com>
2023-07-23 20:53:07 +0800

    New MouseControl Component and new component system MouseControlSystem

---
+++ include/BA/Systems/MouseControlSystem.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "BA/Components/MouseControl.hpp"
+#include "BA/Inputs/MouseInput.hpp"
+#include "BA/Systems/ComponentSystem.hpp"
+
+namespace ba {
+
+class MouseControlSystem : public ComponentSystem {
+public:
+	MouseControlSystem(EntityManager* entityManager);
+
+	virtual void update(float deltaTime);
+
+	virtual void add(std::shared_ptr<Entity>& entity);
+private:
+	MouseInput* m_mouseInput = nullptr;
+
+}; // class ComponentSystem
+
+} // namespace ba
---
+++ src/BA/Systems/MouseControlSystem.cpp
@@ -0,0 +1,55 @@
+#include "BA/Systems/MouseControlSystem.hpp"
+#include "BA/Systems/EntityManager.hpp"
+#include "BA/Entities/Entity.hpp"
+
+namespace ba {
+
+MouseControlSystem::MouseControlSystem(EntityManager* entities) :
+	ComponentSystem(entities)
+{
+
+}
+
+void MouseControlSystem::update(float) {
+	for (IDtype ID : m_entityIDs) {
+		auto e = this->getEntity(ID);
+
+		auto mis = e->getComponent<MouseControl>();
+		for (auto& [btn, actions] : mis->getBindingsOnMouseButtonPressed()) {
+			if (m_mouseInput->isButtonDown(btn)) {
+				for (auto& action : actions) {
+					action();
+				}
+			}
+		}
+
+		for (auto& [btn, actions] : mis->getBindingsOnMouseButtonReleased()) {
+			if (m_mouseInput->isButtonUp(btn)) {
+				for (auto& action : actions) {
+					action();
+				}
+			}
+		}
+
+		for (auto& [btn, actions] : mis->getBindingsOnMouseButtonActive()) {
+			if (m_mouseInput->isButtonActive(btn)) {
+				for (auto& action : actions) {
+					action();
+				}
+			}
+		}
+	}
+
+}
+
+void MouseControlSystem::add(std::shared_ptr<Entity>& entity) {
+	if (entity->getComponent<MouseControl>() != nullptr) {
+		this->addID(entity->ID);
+
+		if (this->m_mouseInput == nullptr) {
+			this->m_mouseInput = entity->CONTEXT->inputs->getInput<MouseInput>().get();
+		}
+	}
+}
+
+} // namespace ba
