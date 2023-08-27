ee8e1d27f0c827cbc73bfa72ad64ce8557d40ced
Charlemagne Javinal <javinal.charlemagne@gmail.com>
2023-08-28 01:47:06 +0800

    Documenation blocks for Entity.

--- include/BA/Entities/Entity.hpp
+++ include/BA/Entities/Entity.hpp
@@ -12,60 +12,160 @@
 
 namespace ba {
 
+/***********************************************************************
+ * @brief A concept ensuring C is a subtype of ba::Component.
+ *
+ * @tparam C A class name that should be subtype of Component.
+************************************************************************/
 template <typename C>
 concept ComponentType = std::is_base_of<ba::Component, C>::value;
 
-class Entity : public Transformable {
-public: // ATTRIBUTES
-	static unsigned count;
-	const unsigned ID;
 
+/***********************************************************************
+ * @brief An object to represent each game entity.
+ *
+************************************************************************/
+class Entity : public Transformable {
+public:
+	/***********************************************************************
+	 * @brief Entity Identification.
+	 *
+	 * Mostly used as keys for maps or sets kept by Systems and Components.
+	************************************************************************/
+	const IDtype ID;
+
+	/***********************************************************************
+	 * @brief A const pointer that allows access to the game's context.
+	 *
+	************************************************************************/
 	const ba::SharedContext* CONTEXT;
-public: // METHODS & CONSTRUCTORS
+public:
+	/***********************************************************************
+	 * @brief Entiy constructor.
+	 *
+	 * @param context sets the public attribute Entity::CONTEXT.
+	************************************************************************/
 	explicit Entity(SharedContext* context);
 
 	/**
 	 * awake()
 	 * Use to ensure that all components are present and all attributes are initialized.
 	*/
+
+	/***********************************************************************
+	 * @brief Use to ensure that all components and their attributes are
+	 * properly initialized.
+	 *
+	************************************************************************/
 	virtual void awake();
 
+	/***********************************************************************
+	 * @brief Checks if this Entity is a static object.
+	 *
+	 * @return true if this object is static, otherwise false.
+	************************************************************************/
 	bool isStatic() const;
+
+	/***********************************************************************
+	 * @brief Declares whether this Entity is a static object.
+	 *
+	 * @param isStatic boolean value.
+	 *
+	 * Static entities are treated differently by Systems from non-static
+	 * entities. Setting an entity as static tells the engine that the state of
+	 * this entity are not likely to transform, and thus not included in system
+	 * loops for efficiency reasons. Example of game objects that should be
+	 * declared statics are. but not limited to, as follows:
+	 * background, non-moving tiles, decoration objects like flower, plants,
+	 * clutter, paths, etc..
+	************************************************************************/
 	void setStatic(bool isStatic);
 
+	/***********************************************************************
+	 * @brief returns a boolean value that tells if this entity is scheduled
+	 * to be removed from the main loop.
+	 *
+	 * @return true if this object is destined for removal, otherwise false.
+	************************************************************************/
 	bool isQueuedForRemoval() const;
+
+	/***********************************************************************
+	 * @brief Sets wether to remove this object or not.
+	 *
+	 * @param remove Boolean value. Sets whether to remove this object.
+	 *
+	 * If given a true value, then the entity manager holding a pointer of
+	 * this entity shall instruct all systems to clear references of this entity
+	 * before eventually freeing memory assigned to this as well as its
+	 * components.
+	 * Giving a false value within the same frametime after it was given a true
+	 * value should prevent that from happening.
+	************************************************************************/
 	void queueForRemoval(bool remove = true);
 
-	/**
-	 * addComponent()
-	 * - Creates a Component of Type T and stores it into this entity's Component array. T must be a subclass of ba::Component. Returns a shared_ptr of the created Component.
-	*/
+	/***********************************************************************
+	 * @brief Creates a ComponentType of type T and stores it in this entity's
+	 * component array and returning a shared pointer to it.
+	 *
+	 * @tparam T The type of Component.
+	 * @return a shared pointer to the created Component.
+	************************************************************************/
 	template<ComponentType T>
 	std::shared_ptr<T> addComponent();
 
-
-	/**
-	 * getComponent()
-	 * - Searches the Component array for an object of type T and return a smart_ptr to it. Returns nullptr if none exist.
-	*/
+	/***********************************************************************
+	 * @brief returns a shared pointer to the requested component. Returns
+	 * nullptr if the component does not exist.
+	 *
+	 * @tparam T The type of Component.
+	 * @return a shared pointer to the requested component or nullptr.
+	************************************************************************/
 	template<ComponentType T>
 	std::shared_ptr<T> getComponent();
 
+	/***********************************************************************
+	 * @brief Returns a pointer to the drawable component of this entity, or
+	 *  nullptr if this entity does not have one. Equivalent to
+	 * @code
+	 * entityObject.getComponent<ba::Drawable>();
+	 * @endcode
+	 *
+	 * @return a pointer to the drawable component, or nullptr.
+	************************************************************************/
 	std::shared_ptr<Drawable> getDrawable() const;
+
+	/***********************************************************************
+	 * @brief Returns to pointer to the collider component of his entity, or
+	 * nullptr if this entity does not have one. Equivalent to
+	 * @code
+	 * entityObject.getComponent<ba::Collider>();
+	 * @endcode
+	 *
+	 * @return a pointer to the collider component, or nullptr.
+	************************************************************************/
 	std::shared_ptr<Collider> getCollider() const;
 
 private: // ATTRIBUTES
 
-	/* Boolean attribute to signal the systems if this entity is subject for destruction.	*/
 	bool m_queuedForRemoval = false;
 
 	bool m_static = false;
 
 	std::map<IDtype, std::shared_ptr<ba::Component>> m_components;
 
-	// std::shared_ptr<Drawable> m_drawable = nullptr;
-	// std::shared_ptr<Collider> m_collider = nullptr;
+private:
+	static IDtype count;
 }; // class Entity
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+//// IMPLEMENTATION IMPLEMENTATION IMPLEMENTATION IMPLEMENTATION   ////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
 
 template<ComponentType T>
 std::shared_ptr<T> Entity::addComponent() {
