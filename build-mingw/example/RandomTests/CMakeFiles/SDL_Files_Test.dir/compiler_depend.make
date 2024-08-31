e4f16ee0e53ea212c09458b046af5df75e84e429
Charlemagne Javinal <javinal.charlemagne@gmail.com>
2024-09-01 02:10:52 +0800

    Used tinyxml2 in ResourceManager::loadConfig().

---
+++ build-mingw/example/RandomTests/CMakeFiles/SDL_Files_Test.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SDL_Files_Test.
+# This may be replaced when dependencies are built.
---
+++ build-mingw/example/Text/CMakeFiles/Text.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Text.dir/TextScene.cpp.obj"
+  "CMakeFiles/Text.dir/TextScene.cpp.obj.d"
+  "CMakeFiles/Text.dir/main.cpp.obj"
+  "CMakeFiles/Text.dir/main.cpp.obj.d"
+  "Text.exe"
+  "Text.pdb"
+  "libText.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Text.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
