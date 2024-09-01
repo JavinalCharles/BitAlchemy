046bd754ca07dc816766be92c46d22f18b219fc9
Charlemagne Javinal <javinal.charlemagne@gmail.com>
2024-09-02 01:07:40 +0800

    Modified ResourceManager::loadXML() and ::getXML() to use tinyxml2::XMLDocument()

---
+++ dirs/bit-alchemy/assets/XML/Interiors_free_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Interiors_free_32x32" tilewidth="32" tileheight="32" tilecount="1424" columns="16">
+ <image source="Modern tiles_Free/Interiors_free/32x32/Interiors_free_32x32.png" width="512" height="2848"/>
+</tileset>
---
+++ dirs/bit-alchemy/assets/XML/Room_Builder_free_32x32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="Room_Builder_free_32x32" tilewidth="32" tileheight="32" tilecount="391" columns="17">
+ <image source="Modern tiles_Free/Interiors_free/32x32/Room_Builder_free_32x32.png" width="544" height="736"/>
+</tileset>
