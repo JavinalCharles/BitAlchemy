15121c83711202a4278e9f0c52bd5b36c0747d3c
Charlemagne Javinal <javinal.charlemagne@gmail.com>
2024-09-06 10:27:02 +0800

    Added some isometric map set in dirs for future usage.

---
+++ dirs/bit-alchemy/assets/XML/32x32tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="32x32tileset" tilewidth="32" tileheight="32" tilecount="121" columns="11">
+ <image source="isometric tileset/spritesheet.png" width="352" height="352"/>
+</tileset>
