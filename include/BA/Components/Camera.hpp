418795eb434ce8b558e8e429600d1a3fbdfa0dd1
JavinalCharles <javinal.charlemagne@gmail.com>
2024-09-16 12:03:02 +0800

    Updated Camera to work with different views or rendering layer, possibly allowing multiple cameras in game.

--- include/BA/Components/Camera.hpp
+++ include/BA/Components/Camera.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "BA/Components/Component.hpp"
-#include "BA/Window/View.hpp"
+#include "BA/Window/Window.hpp"
 
 namespace ba {
 
@@ -9,10 +9,13 @@ class Camera : public Component {
 public:
 	explicit Camera(Entity* owner);
 
+	void useViewFromLayer(IDtype RENDER_LAYER = DEFAULT_RENDER_LAYER);
 	void setView(const View& view);
 	const View& getView() const;
 	void setViewCenter(const Vector2f& center);
 private:
+	bool m_isUsingCustomView = false;
+	ba::IDtype m_layerID = DEFAULT_RENDER_LAYER;
 	ba::View m_cameraView;
 public:
 	static const IDtype CID;
