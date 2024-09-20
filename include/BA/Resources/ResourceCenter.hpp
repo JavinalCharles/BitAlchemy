e9c39257e83fd9eb865d1a9c1a0534be516d8d1b
JavinalCharles <javinal.charlemagne@gmail.com>
2024-09-21 00:49:41 +0800

    Created a Texture wrapper class.

---
+++ include/BA/Resources/ResourceCenter.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <typeindex>
+#include <unordered_map>
+
+#include <SDL2/SDL_filesystem.h>
+
+namespace ba {
+namespace Resources {
+
+class ResourceCenter {
+public:
+
+
+private:
+}; // class ResourceManager
+
+} // namespace Resources
+}; // namespace ba
